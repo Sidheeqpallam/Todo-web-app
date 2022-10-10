7ce252de6c61939ec7bf706701fb600057fd703d
Sidheeq pallam <sidheeqpallam111@gmail.com>
2022-10-10 10:49:28 +0530

    Enter button submit implimented

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import { useState } from "react";
 function App() {
   const [toDos, setToDos] = useState([]);
   const [toDo, setToDo] = useState("");
-  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const now = new Date()
-  const day = days[ now.getDay() ];
+  const day = days[now.getDay()];
+  const submit = (e) => {
+    e.preventDefault()
+    if (toDo.trim() == '') {
+      alert('Enter ToDo')
+    } else {
+      setToDos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+      setToDo('')
+    }
+    
+  }
   return (
-    <div  className="app">
+    <div className="app">
       <div className="mainHeading">
         <h1>ToDo List</h1>
       </div>
@@ -17,36 +27,32 @@ function App() {
         <br />
         <h2>Whoop, it's {day} 🌝 ☕ </h2>
       </div>
-      <div className="input">
-        <input
-          value={toDo}
-          onChange={(e) => {
-            setToDo(e.target.value);
-          }}
-          type="text"
-          placeholder="🖊️ Add item..."
-        />
-        <i
-          onClick={() => {
-            if(toDo.trim() == ''){
-              alert('Enter ToDo')
-            }else{
-              setToDos([...toDos, {id: Date.now(), text : toDo, status : false}]); 
-              setToDo('')
-            }
-            
-          }}
-          className="fas fa-plus"
-        ></i>
-      </div>
+      <form onSubmit={submit}>
+
+        <div className="input">
+          <input
+            value={toDo}
+            onChange={(e) => {
+              setToDo(e.target.value);
+            }}
+            type="text"
+            placeholder="🖊️ Add item..."
+          />
+          <button type="submit" >
+            <i
+
+              className="fas fa-plus"
+            ></i>    </button>
+        </div>
+      </form>
       <div className="todos">
         {toDos.map((obj) => {
           return (
             <div key={obj.id} className={obj.status ? 'todo active' : "todo"}>
-              <div  className="left">
-                <input onChange={(e)=>{
-                  setToDos(toDos.filter(obj2 =>{
-                    if(obj2.id === obj.id){
+              <div className="left">
+                <input onChange={(e) => {
+                  setToDos(toDos.filter(obj2 => {
+                    if (obj2.id === obj.id) {
                       obj2.status = e.target.checked;
                     }
                     return obj2
@@ -54,25 +60,18 @@ function App() {
                 }} value={obj.status} type="checkbox" name="" id="" />
                 <p>{obj.text}</p>
               </div>
-              <div className="right" onClick={()=>
-                setToDos(toDos.filter(obj2=>{
-                  if(obj2.id !== obj.id){
+              <div className="right" onClick={() =>
+                setToDos(toDos.filter(obj2 => {
+                  if (obj2.id !== obj.id) {
                     return obj2
                   }
                 }))
-                } >
+              } >
                 <i className="fas fa-times"></i>
               </div>
             </div>
           );
         })}
-
-        {/* {toDos.map((obj)=>{
-          if(obj.status){
-            return(<h1>{obj.text}</h1>)
-          }
-          return null
-        })} */}
       </div>
     </div>
   );
